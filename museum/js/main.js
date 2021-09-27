a2227c05c202c2e1feaaae684e90c737ca69b22a
Nick1091 <nick10.91@mail.ru>
2021-09-27 14:09:07 +0300

    feat: add some files

---
+++ museum/js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $('.burger').click(function(event) {
+        $('.burger, .navigation').toggleClass('active');
+    });  
+});
+
+$(document).ready(function(){
+    $("#navigation").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
+
+$(document).ready(function(){
+    $("#navig").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
+
+
+
+
