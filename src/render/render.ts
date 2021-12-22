85704604082a5f46a2cd625b55a97fa2e302b130
Nick1091 <nick10.91@mail.ru>
2021-12-22 23:31:59 +0300

    fix: some mistake

--- src/render/render.ts
+++ src/render/render.ts
@@ -62,8 +62,8 @@ class Header {
               <select class="sort-select">
                 <option selected value="sort-name-max">По названию от «А» до «Я»</option>
                 <option value="sort-name-min">По названию от «Я» до «А»</option>
-                <option value="sort-year-max">По по году приобретения первые</option>
-                <option value="sort-year-min">По по году приобретения последние</option>
+                <option value="sort-year-max">По году приобретения первые</option>
+                <option value="sort-year-min">По году приобретения последние</option>
               </select>
               <button class="resetSettings">Сброс настроек</button>
               <button class="reset">Сброс фильтров</button>
