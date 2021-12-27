4f9f94fd62c4436f83626e2162dd5d3f86fc27a2
Nick1091 <nick10.91@mail.ru>
2021-12-28 02:01:21 +0300

    feat: start make garland

--- src/secondPage/treeRender/getGarland/getGarland.ts
+++ src/secondPage/treeRender/getGarland/getGarland.ts
@@ -1,39 +1,58 @@
 import './garland.scss';
 export function getGarland() {
+  // const urlBackground = getUrlBackground(background);
+
+  // const ulList = new Array(20).fill("line");
+  // const listItems= new Array(24).fill("li-element");
+  // const colors = ["red", "yellow", "blue", "green"];
+
+  // function getGarlandColor(garlandSelected: string) {
+  //   switch (garlandSelected) {
+  //     case 'garland-red': return 'red';
+  //     case 'garland-green': return 'green';
+  //     case 'garland-blue': return 'blue';
+  //     case 'garland-yellow': return 'yellow';
+  //     case 'garland-multicolor': return colors[(Math.floor(Math.random() * 4))];
+  //     default: return 'off-light';
+  //   }
+  // }
+
+  // const rot = 32;
+  // const angle = 5;
+  // const circleSize = 500;
+
+  // (((index) >= (1 + ulIndex / 2)) && ((index) <= (22 - ulIndex / 2)))
+
   const turnGarland = document.querySelector('#turn-lights-id') as HTMLInputElement;
   const treeGarland = document.querySelector('.tree-lights-container') as HTMLInputElement;
   turnGarland.addEventListener('click', () => {
     const n = [
-      [65, 5],
-      [60, 9],
-      [60, 13],
-      [60, 18],
-      [55, 20],
-      [55, 25],
-      [58, 20],
-      [58, 25],
+      [88, 5],
+      [80, 9],
+      [86, 13],
+      [80, 18],
+      [82, 18],
+      [78, 20],
+      [70, 28],
+      [74, 38],
     ];
-    for (let i = 0; i < 6; i++) {
-      let rotate = n[i][0];
+    for (let i = 0; i < 8; i++) {
       const listLight = document.createElement('ul');
       listLight.classList.add('chain-garland');
-      // const width = 500;
       const width = 120 + (120 / 2) * i;
       const height = 120 + (120 / 2) * i;
-      // const height = 50 + 100 * i;
       listLight.style.height = `${height}px`;
       listLight.style.width = `${width}px`;
+      let rotate = n[i][0];
       for (let a = 0; a < n[i][1]; a++) {
         const light = document.createElement('li');
-        light.style.transform = `rotate(${rotate}deg) translate(${height}px) rotate(-${rotate}deg)`;
+        light.style.transform = `rotate(${rotate}deg) translate(${height - 50}px) rotate(-${rotate}deg)`;
         light.classList.add('li');
         console.log(rotate);
-        rotate += 4;
+        rotate += 2;
         listLight.append(light);
       }
       treeGarland.append(listLight);
     }
   });
-  // transform: `translateY(${(-0.1 * index * index)}px)`
-  // light.style.transform = rotate(${rotate}deg) translate(250px) rotate(-${rotate}deg)
 }
