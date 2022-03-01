333e803a61ad5c4ee711bf049babfd371e132f9a
Nick1091 <nick10.91@mail.ru>
2022-03-01 23:31:00 +0300

    feat: add timer

---
+++ src/app/timer/timer.ts
@@ -0,0 +1,32 @@
+import Control from '../../common/control';
+
+export class Timer extends Control {
+  onTimeout: () => void;
+
+  timer: number;
+
+  initialTime: number;
+
+  start(time: number) {
+    this.initialTime = time;
+    if (this.timer) {
+      this.stop();
+    }
+    let currentTime = time;
+    const render = (current: number) => {
+      this.node.textContent = `${current <= 9 ? `0${current}` : current} / ${this.initialTime}`;
+    };
+    render(time);
+    this.timer = window.setInterval(() => {
+      currentTime--;
+      render(currentTime);
+      if (currentTime <= 0) {
+        this.onTimeout();
+      }
+    }, 1000);
+  }
+
+  stop() {
+    window.clearInterval(this.timer);
+  }
+}
